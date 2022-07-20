7bd05867377730bc6fb1dc900ac6a40ba5f78d65
robertokbr <robertojuniordev@gmail.com>
2022-07-20 19:39:31 -0300

    style: format code

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
@@ -9,68 +8,65 @@ error Raffle__TransferFailed();
 
 /**
  * @dev this is going to use Chainlink Verifiable Random Function:
- * https://docs.chain.link/docs/intermediates-tutorial/#:~:text=vrf; 
+ * https://docs.chain.link/docs/intermediates-tutorial/#:~:text=vrf;
  * @dev this is going to use Chainlink keepers to trigger the lottery winner function;
  */
 contract Raffle is VRFConsumerBaseV2 {
-  // State variables
-  uint256 private immutable i_entranceFee;
-  address payable[] private s_players;
+    // State variables
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
 
-  // Lotery variables
-  address payable private s_Lastwinner;
+    // Lotery variables
+    address payable private s_Lastwinner;
 
-  /** Events */
-  event RaffleEnter(address indexed player);
-  event RequestedRafflewinner(uint256 indexed requestId);
-  event WinnerPicked(address indexed winner);
+    /** Events */
+    event RaffleEnter(address indexed player);
+    event RequestedRafflewinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
 
-  constructor(
-    address vrfCordinatorV2, 
-    uint256 entranceFee
-  ) VRFConsumerBaseV2(vrfCordinatorV2) {
-    i_entranceFee = entranceFee;
-  }
-
-  function enterRaffle() public payable {
-    if (msg.value < i_entranceFee) {
-      revert Raffle__NotEnoughETHEntered();
+    constructor(address vrfCordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCordinatorV2) {
+        i_entranceFee = entranceFee;
     }
 
-    s_players.push(payable(msg.sender));
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
 
-    emit RaffleEnter(msg.sender);
-  }
+        s_players.push(payable(msg.sender));
 
-  function requestRandomWinner() external {}
-
-  function fulfillRandomWords(
-    uint256 /*requestId*/, 
-    uint256[] memory randomWords
-  ) internal override {
-    uint256 indexOfWinner =  s_players.length % randomWords[0];
-    address payable winner = s_players[indexOfWinner];
-    s_Lastwinner = winner;
-
-    (bool success, ) = winner.call{value: address(this).balance}("");
-
-    if (!success) {
-      revert Raffle__TransferFailed();
+        emit RaffleEnter(msg.sender);
     }
 
-    emit WinnerPicked(winner);
-  }
+    function requestRandomWinner() external {}
 
-  /** Pure | View functions */
-  function getEntranceFee() public view returns (uint256) {
-    return i_entranceFee;
-  }
+    function fulfillRandomWords(
+        uint256, /*requestId*/
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = s_players.length % randomWords[0];
+        address payable winner = s_players[indexOfWinner];
+        s_Lastwinner = winner;
 
-  function getPlayer(uint16 index) public view returns (address) {
-    return s_players[index];
-  }
+        (bool success, ) = winner.call{value: address(this).balance}("");
 
-  function getLastWinner() public view returns (address) {
-    return s_Lastwinner;
-  }
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+
+        emit WinnerPicked(winner);
+    }
+
+    /** Pure | View functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint16 index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getLastWinner() public view returns (address) {
+        return s_Lastwinner;
+    }
 }
